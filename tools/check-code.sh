8dbcf7e7d3dd6d6c3e7626762aca154eb79f47b0
Geoffrey Yu <geoffxy@gmail.com>
2021-01-01 16:14:29 -0500

    [Tools] Allow running parts of check-code.sh for CI use

--- tools/check-code.sh
+++ tools/check-code.sh
@@ -5,37 +5,61 @@ cd $SCRIPT_PATH
 source shared.sh
 cd ..
 
+mode=$1
+check_format="format"
+check_lint="lint"
+check_types="types"
+
+if [ ! -z $mode ] && [ $mode != $check_format ] && [ $mode != $check_lint ] && [ $mode != $check_types ]; then
+  echo "Usage $0 [format|lint|types]"
+  exit 1
+fi
+
 set -e
 
-echo_blue "Tool Versions"
-echo_blue "============="
-black --version
-mypy --version
-pylint --version
+echo_blue "Tooling"
+echo_blue "======="
+if [ -z $mode ] || [ $mode == $check_format ]; then
+  black --version
+fi
+if [ -z $mode ] || [ $mode == $check_lint ]; then
+  pylint --version
+fi
+if [ -z $mode ] || [ $mode == $check_types ]; then
+  mypy --version
+fi
 echo ""
 
 set +e
 
-echo_blue "Check Formatting (black)"
-echo_blue "========================"
-black --check .
-black_exit=$?
-echo ""
+if [ -z $mode ] || [ $mode == $check_format ]; then
+  echo_blue "Check Formatting (black)"
+  echo_blue "========================"
+  black --check .
+  black_exit=$?
+  echo ""
+fi
 
-echo_blue "Lint (pylint)"
-echo_blue "============="
-pylint src/conductor errors/codegen.py setup.py
-pylint_exit=$?
-echo ""
+if [ -z $mode ] || [ $mode == $check_lint ]; then
+  echo_blue "Lint (pylint)"
+  echo_blue "============="
+  pylint src/conductor errors/codegen.py setup.py
+  pylint_exit=$?
+  echo ""
+fi
 
-echo_blue "Type Check (mypy)"
-echo_blue "================="
-mypy src/conductor/__main__.py
-mypy_exit=$?
-echo ""
+if [ -z $mode ] || [ $mode == $check_types ]; then
+  echo_blue "Type Check (mypy)"
+  echo_blue "================="
+  mypy src/conductor/__main__.py
+  mypy_exit=$?
+  echo ""
+fi
 
 function report_status() {
-  if [ $1 -eq 0 ]; then
+  if [ -z $1 ]; then
+    echo "- Skipped"
+  elif [ $1 -eq 0 ]; then
     echo_green "✓ Passed"
   else
     echo_red "✗ Failed"
@@ -48,6 +72,16 @@ echo -n "Formatting  "; report_status $black_exit
 echo -n "Lint        "; report_status $pylint_exit
 echo -n "Type Check  "; report_status $mypy_exit
 
-if [ $black_exit -ne 0 ] || [ $pylint_exit -ne 0 ] || [ $mypy_exit -ne 0 ]; then
-  exit 1
+if [ -z $mode ]; then
+  if [ $black_exit -ne 0 ] || [ $pylint_exit -ne 0 ] || [ $mypy_exit -ne 0 ]; then
+    exit 1
+  fi
+fi
+
+if [ $mode == $check_format ]; then
+  exit $black_exit
+elif [ $mode == $check_lint ]; then
+  exit $pylint_exit
+elif [ $mode == $check_types ]; then
+  exit $mypy_exit
 fi
