bb451de56bd29a4d8f0cd1b1e6cbfa6f0affc9f2
Geoffrey Yu <geoffxy@gmail.com>
2021-02-06 18:19:07 -0500

    [Tasks] Add ability to specify extra command line options in run_experiment()

---
+++ src/conductor/utils/experiment_options.py
@@ -0,0 +1,67 @@
+import pathlib
+import json
+from typing import Dict, Union
+
+from conductor.config import EXP_OPTION_CMD_FORMAT
+from conductor.errors import (
+    ExperimentOptionsNonPrimitiveValue,
+    ExperimentOptionsNonStringKey,
+)
+from conductor.task_identifier import TaskIdentifier
+
+OptionValue = Union[str, bool, int, float]
+
+
+class ExperimentOptions:
+    """
+    Represents key-value options that should be passed to a
+    `run_experiment()` task.
+    """
+
+    def __init__(self, options: Dict[str, OptionValue]):
+        self._options = options
+
+    @classmethod
+    def from_raw(
+        cls, identifier: TaskIdentifier, raw_options: dict
+    ) -> "ExperimentOptions":
+        for key, value in raw_options.items():
+            if not isinstance(key, str):
+                raise ExperimentOptionsNonStringKey(identifier=identifier)
+            if (
+                not isinstance(value, str)
+                and not isinstance(value, bool)
+                and not isinstance(value, int)
+                and not isinstance(value, float)
+            ):
+                raise ExperimentOptionsNonPrimitiveValue(identifier=identifier, key=key)
+        return cls(raw_options)
+
+    def empty(self) -> bool:
+        return len(self._options) == 0
+
+    def serialize_cmdline(self) -> str:
+        """
+        Serializes the options into a form that can be passed to an
+        executable as if it were a command line program.
+        e.g., "--threads=3 --memory=2"
+        """
+        options = []
+        for key, value in self._options.items():
+            if isinstance(value, bool):
+                options.append(
+                    EXP_OPTION_CMD_FORMAT.format(
+                        key=key, value="true" if value else "false"
+                    )
+                )
+            else:
+                options.append(EXP_OPTION_CMD_FORMAT.format(key=key, value=str(value)))
+        return " ".join(options)
+
+    def serialize_json(self, file_path: pathlib.Path) -> None:
+        """
+        Serializes the options into JSON and writes the result to a file at
+        `file_path`.
+        """
+        with open(file_path, "w") as file:
+            json.dump(self._options, file, indent=2, sort_keys=True)
---
+++ tests/experiment_options_test.py
@@ -0,0 +1,119 @@
+import pytest
+
+import json
+import pathlib
+from typing import Dict
+
+from conductor.errors import (
+    ExperimentOptionsNonPrimitiveValue,
+    ExperimentOptionsNonStringKey,
+)
+from conductor.task_identifier import TaskIdentifier
+from conductor.utils.experiment_options import ExperimentOptions, OptionValue
+
+
+def assert_serialized_cmdline(cmd_options: str, raw_options: Dict[str, OptionValue]):
+    pieces = cmd_options.split(" ")
+    assert len(pieces) == len(raw_options)
+    for option in pieces:
+        key, value = option.split("=")
+        key_without_leading = key[2:]
+        assert key_without_leading in raw_options
+        if isinstance(raw_options[key_without_leading], bool):
+            assert value == ("true" if raw_options[key_without_leading] else "false")
+        else:
+            assert value == str(raw_options[key_without_leading])
+
+
+def assert_serialized_json(
+    json_path: pathlib.Path, raw_options: Dict[str, OptionValue]
+):
+    assert json_path.is_file()
+
+    with open(json_path, "r") as file:
+        loaded = json.load(file)
+    assert loaded == raw_options
+
+
+def test_valid(tmp_path: pathlib.Path):
+    simple: Dict[str, OptionValue] = {
+        "hello": 1,
+        "world": "hello",
+        "key": True,
+        "key2": 3.14159,
+    }
+    identifier = TaskIdentifier.from_str("//test:ident")
+    simple_options = ExperimentOptions.from_raw(identifier, simple)
+    assert not simple_options.empty()
+
+    # Test command line serialization
+    cmd_options = simple_options.serialize_cmdline()
+    assert len(cmd_options) > 0
+    assert_serialized_cmdline(cmd_options, simple)
+
+    # Test JSON serialization
+    json_file = tmp_path / "options.json"
+    simple_options.serialize_json(json_file)
+    assert_serialized_json(json_file, simple)
+
+
+def test_single_option(tmp_path: pathlib.Path):
+    single: Dict[str, OptionValue] = {
+        "key": "value",
+    }
+    identifier = TaskIdentifier.from_str("//test:ident")
+    options = ExperimentOptions.from_raw(identifier, single)
+    assert not options.empty()
+
+    # Test command line serialization
+    cmd_options = options.serialize_cmdline()
+    assert len(cmd_options) > 0
+    assert_serialized_cmdline(cmd_options, single)
+
+    # Test JSON serialization
+    json_file = tmp_path / "options.json"
+    options.serialize_json(json_file)
+    assert_serialized_json(json_file, single)
+
+
+def test_empty():
+    empty: Dict[str, OptionValue] = {}
+    identifier = TaskIdentifier.from_str("//test:ident")
+    options = ExperimentOptions.from_raw(identifier, empty)
+    assert options.empty()
+    assert options.serialize_cmdline() == ""
+
+
+def test_non_string_key():
+    numeric_key = {
+        "hello": "world",
+        3: "asd",
+    }
+    tuple_key = {
+        (1, 2): "test",
+        "foo": "bar",
+    }
+    identifier = TaskIdentifier.from_str("//test:ident")
+    with pytest.raises(ExperimentOptionsNonStringKey):
+        ExperimentOptions.from_raw(identifier, numeric_key)
+    with pytest.raises(ExperimentOptionsNonStringKey):
+        ExperimentOptions.from_raw(identifier, tuple_key)
+
+
+def test_non_primitive_value():
+    nested_value = {
+        "hello": "world",
+        "nested": {
+            "value": 1,
+        },
+    }
+    tuple_value = {
+        "value": (1, 2, 3),
+        "key": "value",
+    }
+
+    identifier = TaskIdentifier.from_str("//test:ident")
+    with pytest.raises(ExperimentOptionsNonPrimitiveValue):
+        ExperimentOptions.from_raw(identifier, nested_value)
+    with pytest.raises(ExperimentOptionsNonPrimitiveValue):
+        ExperimentOptions.from_raw(identifier, tuple_value)
