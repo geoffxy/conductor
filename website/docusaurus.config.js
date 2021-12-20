dc5aba84ba96af8cb923e7301fcefbc866e9e241
Geoffrey Yu <geoffxy@gmail.com>
2021-12-20 00:04:05 -0500

    Add Conductor logo and favicon

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -6,7 +6,7 @@ module.exports = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/ConductorFavicon64.png',
   organizationName: 'geoffxy',
   projectName: 'conductor',
   themeConfig: {
@@ -14,7 +14,8 @@ module.exports = {
       title: 'Conductor',
       logo: {
         alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        src: 'img/ConductorLogo_Light.png',
+        srcDark: 'img/ConductorLogo_Dark.png',
       },
       items: [
         {
