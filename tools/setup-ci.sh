c4c28bf24236fcdf9786b8691926654b4c65eeff
Geoffrey Yu <geoffxy@gmail.com>
2021-12-24 17:51:51 -0500

    Add custom setup script for the CI

---
+++ tools/setup-ci.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+# Runs any setup code needed for our CI.
+
+# Configure git bceause we use it in our tests.
+git config --global user.name "Conductor CI"
+git config --global user.email "<>"
