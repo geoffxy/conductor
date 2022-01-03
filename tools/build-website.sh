592281ea3e7adffbc91c509d8ccca7eda5c55743
Geoffrey Yu <geoffxy@gmail.com>
2022-01-02 19:55:41 -0500

    Handle options in build script based on version

--- tools/build-website.sh
+++ tools/build-website.sh
@@ -13,9 +13,15 @@ if [ "$1" == "--install-deps" ]; then
   npm ci
 fi
 
+node_version=$(node --version)
+
 # Build the website.
-# See https://github.com/webpack/webpack/issues/14532 for details about
-# `--openssl-legacy-provider`.
-NODE_OPTIONS=--openssl-legacy-provider npm run build
+if [[ "$node_version" =~ ^v17\..+$ ]]; then
+  # See https://github.com/webpack/webpack/issues/14532 for details about
+  # `--openssl-legacy-provider`.
+  NODE_OPTIONS=--openssl-legacy-provider npm run build
+else
+  npm run build
+fi
 
 echo_green "✓ Done"
