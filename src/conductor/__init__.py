dc9a82eb69d5005f379083e86d5f1a0ed27abb2d
Geoffrey Yu <geoffxy@gmail.com>
2022-01-15 20:34:23 -0500

    [Docs] Update metadata and README to include link to docs (#57)

--- src/conductor/__init__.py
+++ src/conductor/__init__.py
@@ -4,3 +4,4 @@ __author__ = "Geoffrey Yu"
 __email__ = "geoffxy@gmail.com"
 
 __description__ = "A simple and elegant research computing orchestrator."
+__url__ = "https://www.geoffreyyu.com/conductor/"
