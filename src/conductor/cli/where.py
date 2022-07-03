e2eaf3b68ac5daff3e243fe85116ff69240c786a
Geoffrey Yu <geoffxy@gmail.com>
2022-07-02 23:23:56 -0400

    Add option to always print path if it is known

--- src/conductor/cli/where.py
+++ src/conductor/cli/where.py
@@ -21,6 +21,14 @@ def register_command(subparsers):
         help="If set, Conductor will print the output path relative to the project "
         "root.",
     )
+    parser.add_argument(
+        "-f",
+        "--non-existent-ok",
+        action="store_true",
+        help="If set, Conductor will print the output path even if it does not exist "
+        "yet. This flag only has an effect for tasks that have deterministic output "
+        "paths (e.g., run_command() or combine()).",
+    )
     parser.set_defaults(func=main)
 
 
@@ -34,7 +42,7 @@ def main(args):
     ctx.task_index.load_single_task(task_identifier)
     task = ctx.task_index.get_task(task_identifier)
     output_path = task.get_output_path(ctx)
-    if output_path is None or not output_path.exists():
+    if output_path is None or (not output_path.exists() and not args.non_existent_ok):
         raise NoTaskOutputPath(task_identifier=str(task_identifier))
     if args.project:
         print(output_path.relative_to(ctx.project_root))
