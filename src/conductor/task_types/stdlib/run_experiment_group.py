81abb1a143da97a01c687503047b9e2d73529cf0
Geoffrey Yu <geoffxy@gmail.com>
2023-08-21 16:50:54 -0400

    Add support for chained experiment instances

--- src/conductor/task_types/stdlib/run_experiment_group.py
+++ src/conductor/task_types/stdlib/run_experiment_group.py
@@ -18,10 +18,12 @@ def run_experiment_group(
     name: str,
     run: str,
     experiments: Iterable[ExperimentInstance],
+    chain_experiments: bool = False,
     deps: Optional[Sequence[str]] = None,
 ) -> None:
     task_deps = deps if deps is not None else []
     relative_experiment_identifiers = []
+    prev_experiment_identifier: Optional[str] = None
 
     try:
         seen_experiment_names = set()
@@ -34,6 +36,13 @@ def run_experiment_group(
                 )
 
             seen_experiment_names.add(experiment.name)
+
+            # Add the previously-processed task as a dependency if
+            # `chain_experiments` is set to `True`.
+            experiment_deps = task_deps
+            if chain_experiments and prev_experiment_identifier is not None:
+                experiment_deps = [*task_deps, prev_experiment_identifier]
+
             # run_experiment(): Defined by Conductor at runtime
             # pylint: disable=undefined-variable
             run_experiment(  # type: ignore
@@ -42,9 +51,11 @@ def run_experiment_group(
                 parallelizable=experiment.parallelizable,
                 args=experiment.args,
                 options=experiment.options,
-                deps=task_deps,
+                deps=experiment_deps,
             )
-            relative_experiment_identifiers.append(":" + experiment.name)
+            experiment_identifier = ":" + experiment.name
+            relative_experiment_identifiers.append(experiment_identifier)
+            prev_experiment_identifier = experiment_identifier
 
     except TypeError as ex:
         raise ExperimentGroupInvalidExperimentInstance(task_name=name) from ex
