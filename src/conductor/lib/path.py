54e371220e9091940746351c18272e2d192f0a43
Geoffrey Yu <geoffxy@gmail.com>
2024-03-03 19:11:51 -0500

    [Lib] Add where() helper function to locate experiment results

--- src/conductor/lib/path.py
+++ src/conductor/lib/path.py
@@ -1,12 +1,14 @@
 import os
 import pathlib
-from typing import List, Union
+from typing import List, Union, Optional
 
 from conductor.config import (
     DEPS_ENV_VARIABLE_NAME,
     DEPS_ENV_PATH_SEPARATOR,
     OUTPUT_ENV_VARIABLE_NAME,
 )
+from conductor.context import Context
+from conductor.task_identifier import TaskIdentifier
 
 
 def get_deps_paths() -> List[pathlib.Path]:
@@ -53,3 +55,33 @@ def in_output_dir(file_path: Union[pathlib.Path, str]) -> pathlib.Path:
         return pathlib.Path(file_path)
     else:
         return file_path
+
+
+def where(
+    identifier: str, relative_to_project_root: bool = False, non_existent_ok: bool = False
+) -> Optional[pathlib.Path]:
+    """
+    Returns the output location path of the given task identifier. If this
+    returns `None`, it indicates no output location is available (e.g., the task
+    has not run before).
+
+    If `relative_to_project_root` is set to True, this will return a relative
+    path to the project root. Otherwise, it returns an absolute path.
+
+    If `non_existent_ok` is set to True, this will return the task's output path
+    even if the path does not yet exist.
+    """
+    ctx = Context.from_cwd()
+    task_identifier = TaskIdentifier.from_str(
+        identifier,
+        require_prefix=False,
+    )
+    ctx.task_index.load_single_task(task_identifier)
+    task = ctx.task_index.get_task(task_identifier)
+    output_path = task.get_output_path(ctx)
+    if output_path is None or (not output_path.exists() and not non_existent_ok):
+        return None
+    if relative_to_project_root:
+        return output_path.relative_to(ctx.project_root)
+    else:
+        return output_path
