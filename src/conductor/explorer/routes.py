3224dd04c8513dfec1295e23dfa1609991f82b4a
Geoffrey Yu <geoffxy@gmail.com>
2024-11-25 13:40:37 -0500

    Add front end boilerplate

--- src/conductor/explorer/routes.py
+++ src/conductor/explorer/routes.py
@@ -1,5 +1,10 @@
+import importlib.resources as pkg_resources
+
 from pydantic import BaseModel
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+import conductor.explorer.static as explorer_app
 
 app = FastAPI()
 
@@ -11,3 +16,10 @@ class Simple(BaseModel):
 @app.get("/api/1/hello")
 def hello_world() -> Simple:
     return Simple(message="Hello, World!")
+
+
+# Serve the static pages.
+# Note that this should go last as a "catch all" route.
+static_files = pkg_resources.files(explorer_app)
+with pkg_resources.as_file(static_files) as static_dir:
+    app.mount("/", StaticFiles(directory=static_dir, html=True), name="static")
