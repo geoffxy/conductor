f84b2d64a28db1ee3f3c292c584bd948e9ea8516
Geoffrey Yu <geoffxy@gmail.com>
2025-01-01 22:58:52 -0500

    Add boilerplate for optimizer rules, add env to run_experiment_group()

---
+++ src/conductor/execution/optim_rules/rule.py
@@ -0,0 +1,16 @@
+from typing import Tuple
+
+from conductor.execution.plan import ExecutionPlan
+
+
+class OptimizerRule:
+    """
+    Represents an optimization rule that can be applied to an execution plan.
+    """
+
+    def apply(self, plan: ExecutionPlan) -> Tuple[bool, ExecutionPlan]:
+        """
+        Attempt to apply this rule to the given plan. Returns a tuple of whether
+        or not the rule modified the plan and the modified (or original) plan.
+        """
+        raise NotImplementedError
